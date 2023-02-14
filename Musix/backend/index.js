c724c42f9051bf9c56841acc589109a0ed8956e1
Siddharth <sidyyadav7899@gmail.com>
2023-02-15 00:34:21 +0530

    Adding some files

--- Musix/backend/index.js
+++ Musix/backend/index.js
@@ -22,7 +22,9 @@ app.use(cors({
   
 app.use("/user",userRoutes);
 app.use("/song",songRoutes);
-
+app.get("/",(req,res)=>{
+    res.send("APP IS RUNNING.");
+});
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser: true, useUnifiedTopology: true})
